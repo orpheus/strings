3e37041a35da4cfea0fa99ddbac7afabfb5debe8
ohryan <ryanachacon@gmail.com>
2023-09-26 07:32:36 -0700

    Dockerfile

--- go.mod
+++ go.mod
@@ -6,12 +6,9 @@ require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
 	github.com/gofrs/uuid v4.2.0+incompatible
-	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jackc/pgconn v1.11.0
 	github.com/jackc/pgtype v1.10.0
 	github.com/jackc/pgx/v4 v4.15.0
-	github.com/pkg/errors v0.9.1
-	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
 )
 
 require (
@@ -31,7 +28,9 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- main.go
+++ main.go
@@ -4,34 +4,37 @@ import (
 	"fmt"
 	"github.com/orpheus/strings/infrastructure/postgres"
 	"github.com/orpheus/strings/infrastructure/server"
-	"os"
+	"github.com/orpheus/strings/util"
+	"log"
 )
 
-func getEnv(key, fallback string) string {
-	value, exists := os.LookupEnv(key)
-	if !exists {
-		value = fallback
-	}
-	return value
-}
-
 func main() {
-	dbUser := getEnv("DB_USER", "postgres")
-	dbPass := getEnv("DB_PASS", "")
-	dbName := getEnv("DB_NAME", "strings")
-	dbHost := getEnv("DB_HOST", "localhost")
-	dbPort := getEnv("DB_PORT", "5432")
+	log.Println("Starting server...")
+
+	dbUser := util.GetEnv("DB_USER", "postgres")
+	dbPass := util.GetEnv("DB_PASS", "")
+	dbName := util.GetEnv("DB_NAME", "strings")
+	dbHost := util.GetEnv("DB_HOST", "localhost")
+	dbPort := util.GetEnv("DB_PORT", "5432")
+
+	log.Println("Connecting database...")
 
 	jdbcUrl := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
 	conn := postgres.NewPgxPool(jdbcUrl)
 	defer conn.Close()
+
+	log.Println("Running migrations...")
+
 	postgres.Migrate(conn)
+
+	log.Println("Creating gin server router...")
 
 	s := server.NewGin()
 	server.Construct(s, conn)
 
+	log.Println("Running server...")
 	err := s.Run()
 	if err != nil {
-		panic("Error starting server")
+		log.Fatalln("Error starting server")
 	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
